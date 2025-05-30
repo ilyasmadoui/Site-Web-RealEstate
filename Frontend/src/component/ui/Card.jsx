5db3859b4d3576b3cebaace4e6f8211dc077abef
ilyasmadoui <ilyasmadoui2020@gmail.com>
2025-05-30 23:58:54 +0200

    Add files via upload

---
+++ Frontend/src/component/ui/Card.jsx
@@ -0,0 +1,9 @@
+import "../ui/Card.scss";
+
+export function Card({ children, className }) {
+  return <div className={`card ${className}`}>{children}</div>;
+}
+
+export function CardContent({ children }) {
+  return <div className="card-content">{children}</div>;
+}
